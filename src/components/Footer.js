2c798b538a7e6aa4c52b2f867b6aecdcb51de253
Miriam Wienholt <miriamwienholt@miriams-air.speedport_w_724v_09011603_06_003>
2019-09-09 22:37:44 +0200

    Add footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Footer() {
-  return <footer className="App-footer">Impressum</footer>;
+  return <footer className="App-footer">Christin Grunau  --  Miriam Wienholt</footer>;
 }
 
 export default Footer;
